7e9abb65984cbe9a6e1bc48a2b21c7282a99bead
chenxinzhou <chenxinzhou@antiy.cn>
2017-12-06 11:49:49 +0800

    use mocha for test

--- src/utils/tools/test/queryString.test.ts
+++ src/utils/tools/test/queryString.test.ts
@@ -1,12 +1,12 @@
 import queryString from '../queryString'
-import chai from 'chai'
+import * as chai from 'chai'
 
 const { expect } = chai
 
 
 describe("utils/tools/queryString test", () => {
   it("queryString.ArrayMode should be a object", () => {
-    expect(queryString.ArrayMode).to.be.depp.equal({
+    expect(queryString.ArrayMode).to.be.deep.equal({
       MUL: "multiple",
       SPLIT: "split"
     })
